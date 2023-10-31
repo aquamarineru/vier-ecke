36ad331dd2022f617b81212cb317a0d78f24833d
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-10-31 17:13:06 +0100

    services grid

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,12 +3,14 @@ import { Container, Title, ServicesGrid } from '../components'
 
 export default function Services() {
   return (
-    <div className='bg-green-gradient h-screen w-full py-10 md:pt-24'>
+    <div className='bg-green-gradient h-full w-full py-10 md:pt-24'>
         <Container className='flex flex-col items-center'>
             <Title>
                 Unsere Services
             </Title>
             <Title type='small'>Folgende Leistungen können bei uns angefragt und erbracht werden:</Title>
+            <div></div>
+            <ServicesGrid />
         </Container>
     </div>
   )
--- src/components/ServicesGrid.jsx
+++ src/components/ServicesGrid.jsx
@@ -5,7 +5,7 @@ import { Title } from './'
 
 export default function ServicesGrid() {
   return (
-    <div className='grid grid-cols-3 grid-rows-2 gap-5'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-5 mt-10 md:px-3'>
       <Image
       src='/images/services.png'
       alt='Vier Ecke Services Illustration'
@@ -14,35 +14,35 @@ export default function ServicesGrid() {
       className='object-cover object-center w-full h-full'
       sizes='(width: 300px) 100vw, 800px'
       />
-      <div>
+      <div className='flex flex-col justify-center gap-3'>
         <Title type='small'>
         RENOVIERUNGSARBEITEN
         </Title>
-        <p className='font-light md:text-lg'>
+        <p className='font-light text-sm md:text-base'>
         Wir übernehmen für Sie umfangreiche Renovierungsarbeiten, um Ihr Zuhause oder Geschäftsräume in neuem Glanz erstrahlen zu lassen. Unsere Fachkräfte arbeiten sorgfältig und zuverlässig – egal ob es sich um Malerarbeiten, Bodenbelagswechsel oder andere Instandsetzungsmaßnahmen handelt. Wir setzen Ihre Wünsche mit Präzision um.
         </p>
       </div>
-      <div>
+      <div className='flex flex-col justify-center gap-3  '>
         <Title type='small'>
         RENOVIERUNGSARBEITEN
         </Title>
-        <p className='font-light md:text-lg'>
+        <p className='font-light text-sm md:text-base'>
         Wir übernehmen für Sie umfangreiche Renovierungsarbeiten, um Ihr Zuhause oder Geschäftsräume in neuem Glanz erstrahlen zu lassen. Unsere Fachkräfte arbeiten sorgfältig und zuverlässig – egal ob es sich um Malerarbeiten, Bodenbelagswechsel oder andere Instandsetzungsmaßnahmen handelt. Wir setzen Ihre Wünsche mit Präzision um.
         </p>
       </div>
-      <div>
+      <div className='flex flex-col justify-center gap-3  '>
         <Title type='small'>
         RENOVIERUNGSARBEITEN
         </Title>
-        <p className='font-light md:text-lg'>
+        <p className='font-light text-sm md:text-base'>
         Wir übernehmen für Sie umfangreiche Renovierungsarbeiten, um Ihr Zuhause oder Geschäftsräume in neuem Glanz erstrahlen zu lassen. Unsere Fachkräfte arbeiten sorgfältig und zuverlässig – egal ob es sich um Malerarbeiten, Bodenbelagswechsel oder andere Instandsetzungsmaßnahmen handelt. Wir setzen Ihre Wünsche mit Präzision um.
         </p>
       </div>
-      <div>
+      <div className='flex flex-col justify-center gap-3  '>
         <Title type='small'>
         RENOVIERUNGSARBEITEN
         </Title>
-        <p className='font-light md:text-lg'>
+        <p className='font-light text-sm md:text-base'>
         Wir übernehmen für Sie umfangreiche Renovierungsarbeiten, um Ihr Zuhause oder Geschäftsräume in neuem Glanz erstrahlen zu lassen. Unsere Fachkräfte arbeiten sorgfältig und zuverlässig – egal ob es sich um Malerarbeiten, Bodenbelagswechsel oder andere Instandsetzungsmaßnahmen handelt. Wir setzen Ihre Wünsche mit Präzision um.
         </p>
       </div>
