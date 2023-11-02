872c20f0a874683565ef8afcb9a4d417deb9c3c2
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-11-02 12:40:43 +0100

    layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import Layout from '@/components/Layout'
-import { Analytics } from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
